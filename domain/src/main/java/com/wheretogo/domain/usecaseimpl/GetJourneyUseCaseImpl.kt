2458d71d6e4961d7de762642c63291bc9fe2a57a
dhkim139 <dhkim139@gmail.com>
2024-10-11 18:34:13 +0900

    폴더이동

--- domain/src/main/java/com/wheretogo/domain/usecaseimpl/GetJourneyUseCaseImpl.kt
+++ domain/src/main/java/com/wheretogo/domain/usecaseimpl/GetJourneyUseCaseImpl.kt
@@ -1,8 +1,9 @@
-package com.wheretogo.domain.usecase
+package com.wheretogo.domain.usecaseimpl
 
 import com.wheretogo.domain.repository.JourneyRepository
 import com.wheretogo.domain.model.Course
 import com.wheretogo.domain.model.Journey
+import com.wheretogo.domain.usecase.GetJourneyUseCase
 import javax.inject.Inject
 
 class GetJourneyUseCaseImpl @Inject constructor(private val journeyRepository: JourneyRepository) :
