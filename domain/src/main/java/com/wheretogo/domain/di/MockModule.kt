9ae66ece77cfa21719cd1e215c74a7d338f018b3
dhkim139 <dhkim139@gmail.com>
2024-10-11 18:39:08 +0900

    useCaseImpl git 업로드 제외

--- domain/src/main/java/com/wheretogo/domain/di/MockModule.kt
+++ domain/src/main/java/com/wheretogo/domain/di/MockModule.kt
@@ -7,14 +7,17 @@ import dagger.hilt.components.SingletonComponent
 
 /*
 * 실제 구현체는 git에 업로드 되지 않음
-* todo 빌드오류시 주석을 해제해 사용
+* todo usecaseimpl 없을때 주석을 해제해 사용
 * */
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class MockModule {
 
-  /*  @Binds
-    abstract fun bindGetNearByJourneyUseCase(useCaseImpl: MockGetNearByJourneyUseCaseImpl): GetNearByJourneyUseCase
+  /*
+    @Binds
+    abstract fun bindGetJourneyUseCase(useCaseImpl: GetJourneyUseCaseImpl): GetJourneyUseCase
 
+    @Binds
+    abstract fun bindGetNearByJourneyUseCase(useCaseImpl: MockGetNearByJourneyUseCaseImpl): GetNearByJourneyUseCase
 */
 }
