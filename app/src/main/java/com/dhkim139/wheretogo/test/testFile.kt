c29203322d9ac74af259361bcf1ac41e4ebdf837
dhkim139 <dhkim139@gmail.com>
2024-10-31 17:20:28 +0900

    테스트 - 테스트 이미지 압축시 가로세로 정보 확인 추가

--- app/src/main/java/com/dhkim139/wheretogo/test/testFile.kt
+++ app/src/main/java/com/dhkim139/wheretogo/test/testFile.kt
@@ -3,9 +3,12 @@ package com.dhkim139.wheretogo.test
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
@@ -15,39 +18,48 @@ import java.io.InputStream
 fun createDummy(context: Context) {
     CoroutineScope(Dispatchers.IO).launch {
         context.apply {
-            cacheScaleImageFromAsset("photo_original",100,75,50)
-            cacheScaleImageFromAsset("photo_original",100,75,60)
-            cacheScaleImageFromAsset("photo_original",100,75,70)
-            cacheScaleImageFromAsset("photo_original",100,75,80)
-            cacheScaleImageFromAsset("photo_original",200,150,70)
-            cacheScaleImageFromAsset("photo_original",200,150,80)
-            cacheScaleImageFromAsset("photo_original",1024,768,70)
-            cacheScaleImageFromAsset("photo_original",1024,768,80)
-            cacheScaleImageFromAsset("photo_original",1280,960,70)
-            cacheScaleImageFromAsset("photo_original",1280,960,80)
+            runBlocking {
+                cacheScaleImageFromAsset("photo_original",75,100,50)
+                cacheScaleImageFromAsset("photo_original",75,100,60)
+                cacheScaleImageFromAsset("photo_original",75,100,70)
+                cacheScaleImageFromAsset("photo_original",75,100,80)
+                cacheScaleImageFromAsset("photo_original",150,200,70)
+                cacheScaleImageFromAsset("photo_original",150,200,80)
+                cacheScaleImageFromAsset("photo_original",768,1024,70)
+                cacheScaleImageFromAsset("photo_original",768,1024,80)
+                cacheScaleImageFromAsset("photo_original",960,1280,70)
+                cacheScaleImageFromAsset("photo_original",960,1280,80)
+            }
         }
     }
 }
 
-fun Context.cacheScaleImageFromAsset(source: String, scale:Double){
-    val assetManager = this.assets
-    val inputStream: InputStream = assetManager.open("$source.jpg")
-    val bitmap = BitmapFactory.decodeStream(inputStream)
-    val width = (bitmap.width * scale).toInt()
-    val height = (bitmap.height * scale).toInt()
-    val compress  = 80
-    val resizedBitmap = Bitmap.createScaledBitmap(bitmap, width, height, true)
-    val outputStream = ByteArrayOutputStream()
-    resizedBitmap.compress(Bitmap.CompressFormat.JPEG, compress, outputStream)
-    val scaledData = outputStream.toByteArray()
-    saveCache(scaledData,"${source}_to_scale_1_2_comp_$compress.jpg")
-}
-
 fun Context.cacheScaleImageFromAsset(source: String, w: Int, h: Int, compress: Int) {
+    val path = "$source.jpg"
     val assetManager = this.assets
-    val inputStream: InputStream = assetManager.open("$source.jpg")
-    val bitmap = BitmapFactory.decodeStream(inputStream)
-    val resizedBitmap = Bitmap.createScaledBitmap(bitmap, w, h, true)
+
+    val bitmap = assetManager.open(path).use { inputStream->
+        BitmapFactory.decodeStream(inputStream)
+    }
+    val rotation = assetManager.open(path).use { inputStream->
+        ExifInterface(inputStream).run {
+            when(getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)){
+                ExifInterface.ORIENTATION_ROTATE_90 -> 90
+                ExifInterface.ORIENTATION_ROTATE_180 -> 180
+                ExifInterface.ORIENTATION_ROTATE_270 -> 270
+                else -> 0
+            }
+        }
+    }
+    val rotatedBitmap = if (rotation != 0) {
+        val matrix = Matrix().apply { postRotate(rotation.toFloat()) }
+        Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+    } else {
+        bitmap
+    }
+
+
+    val resizedBitmap = Bitmap.createScaledBitmap(rotatedBitmap, w, h, true)
     val outputStream = ByteArrayOutputStream()
     resizedBitmap.compress(Bitmap.CompressFormat.JPEG, compress, outputStream)
     val scaledData = outputStream.toByteArray()
