4b71529b1e33fd6652e9640fb05142a86120a6d0
dhkim139 <dhkim139@gmail.com>
2025-05-25 04:13:44 +0900

    v1.2.0-rc10 - 비공개 테스트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.dhkim139.wheretogo"
         minSdk = 24
         targetSdk = 34
-        versionCode = 20
-        versionName = "1.2.0-rc9"
+        versionCode = 21
+        versionName = "1.2.0-rc10"
 
         testInstrumentationRunner = "com.dhkim139.wheretogo.TestRunner"
         vectorDrawables {
