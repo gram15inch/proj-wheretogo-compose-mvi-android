8c7573a0e331992bdbf07a0c0473f702792aafcc
dhkim139 <dhkim139@gmail.com>
2025-10-27 12:10:25 +0900

    chore: v1.0-rc5 - 공개 테스트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.dhkim139.wheretogo"
         minSdk = AndroidConfig.MIN_SDK
         targetSdk = AndroidConfig.TARGET_SDK
-        versionCode = 33
-        versionName = "1.0-rc4"
+        versionCode = 34
+        versionName = "1.0-rc5"
 
         testInstrumentationRunner = "com.dhkim139.wheretogo.TestRunner"
         vectorDrawables {
