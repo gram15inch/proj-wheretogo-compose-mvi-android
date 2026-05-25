90e41baa1009d818865cd8abe03693787f612789
dhkim139 <dhkim139@gmail.com>
2026-05-25 12:43:32 +0900

    refact: 필요 하지않는 suspend 키워드 삭제

--- domain/src/main/java/com/wheretogo/domain/usecase/course/FilterListCourseUseCase.kt
+++ domain/src/main/java/com/wheretogo/domain/usecase/course/FilterListCourseUseCase.kt
@@ -4,7 +4,7 @@ import com.wheretogo.domain.model.course.Course
 import com.wheretogo.domain.model.util.Viewport
 
 interface FilterListCourseUseCase {
-    suspend operator fun invoke(
+    operator fun invoke(
         vp: Viewport,
         zoom: Double,
         courseGroup: List<Course>
