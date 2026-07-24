f17f34c9e339689925a0a826643873ba2587a957
dhkim139 <dhkim139@gmail.com>
2026-07-24 23:12:34 +0900

    chore: v1.0-rc25 - 공개 테스트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
 
     defaultConfig {
         applicationId = "com.dhkim139.wheretogo"
-        versionCode = 53
-        versionName = "1.0-rc24"
+        versionCode = 54
+        versionName = "1.0-rc25"
 
         testInstrumentationRunner = "com.dhkim139.wheretogo.TestRunner"
         vectorDrawables {
