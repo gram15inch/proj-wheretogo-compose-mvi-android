823d9b04e48f911a9d414cd159f50b32b76f4d34
dhkim139 <dhkim139@gmail.com>
2026-07-25 23:26:10 +0900

    feat: kotlin 2.2.20

--- build-logic/src/main/kotlin/wheretogo.android.application.gradle.kts
+++ build-logic/src/main/kotlin/wheretogo.android.application.gradle.kts
@@ -10,15 +10,10 @@ android {
         minSdk = 24
         targetSdk = 35
     }
+}
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+kotlin {
+    jvmToolchain(17)
 }
 
 tasks.withType<Test>().configureEach {
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.plugin.compose) apply false
-    id("com.google.dagger.hilt.android") version "2.52" apply false
+    alias(libs.plugins.dagger.hilt.android) apply false
+    alias(libs.plugins.devtools.ksp) apply false
     id("de.mannodermaus.android-junit5") version "1.11.2.0" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.crashlytics") version "3.0.2" apply false
     kotlin("plugin.serialization") version "1.8.0" apply false
@@ -15,5 +15,3 @@ plugins {
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
-
-
