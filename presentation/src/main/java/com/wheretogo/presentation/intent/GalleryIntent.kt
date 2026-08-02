f66282accb008062f58ed643683e2341ccd828da
dhkim139 <dhkim139@gmail.com>
2026-08-02 17:57:12 +0900

    feat: 기존 피커를 카메라 피커로 대체
    - 갤러리형 피커는 관리자용으로 숨김

--- presentation/src/main/java/com/wheretogo/presentation/intent/GalleryIntent.kt
+++ presentation/src/main/java/com/wheretogo/presentation/intent/GalleryIntent.kt
@@ -7,10 +7,12 @@ sealed interface GalleryIntent {
     data class Initialize(val openPicker: Boolean) : GalleryIntent
     data object Refresh : GalleryIntent
     data class MediaPicked(val images: List<PickedImage>) : GalleryIntent
+    data class CameraPicked(val images: List<PickedImage>) : GalleryIntent
     data class ChangeGrouping(val strategy: GroupingStrategy) : GalleryIntent
     data class OpenDetail(val id: Long) : GalleryIntent
     data object CloseDetail : GalleryIntent
     data object PhotoAddClick : GalleryIntent
+    data object GroupingButtonLongClick : GalleryIntent
     data class PhotoClick(val pickerId: Long) : GalleryIntent
     data class PhotoLongClick(val pickerId: Long) : GalleryIntent
     data object ClearSelection : GalleryIntent
