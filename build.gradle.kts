54227281cdbeb15fc6fa3bb06ba2fbc140f98833
dhkim139 <dhkim139@gmail.com>
2026-08-05 11:36:49 +0900

    feat: retrace(복호화) gradle task 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.plugin.compose) apply false
     alias(libs.plugins.dagger.hilt.android) apply false
     alias(libs.plugins.devtools.ksp) apply false
@@ -10,6 +9,20 @@ plugins {
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.crashlytics") version "3.0.2" apply false
     kotlin("plugin.serialization") version "1.8.0" apply false
+}
+
+val r8 by configurations.creating
+dependencies {
+    r8(libs.android.tools.r8)
+}
+
+tasks.register<JavaExec>("retrace") {
+    classpath = r8
+    mainClass.set("com.android.tools.r8.retrace.Retrace")
+    args(
+        "app/build/outputs/mapping/release/mapping.txt",
+        "crash.txt"
+    )
 }
 
 tasks.register("clean", Delete::class) {
